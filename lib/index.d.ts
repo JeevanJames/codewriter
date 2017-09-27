3a90c4d714b023092525e9cf3215077206a5e31c
Jeevan James <jeevanjj@live.com>
2017-09-28 00:56:28 +0530

    Reverted back to a single .ts module till I can solve the multiple definition file issue.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,3 @@
-import { CodeWriterOptions } from './code-writer-options';
 export declare class CodeWriter {
     private code;
     private currentLine;
@@ -115,3 +114,37 @@ export declare class CodeWriter {
      */
     toCode(): string;
 }
+/**
+ * Options to initialize and configure the behavior of a CodeWriter instance.
+ */
+export interface CodeWriterOptions {
+    /**
+     * Optional code to initialize the CodeWriter with.
+     */
+    initialCode?: InitialCode;
+    /**
+     * The indentation size in spaces. Defaults to 4 if not specified.
+     */
+    indentSize?: number;
+    /**
+     * Function that can format a given string as a language-specific single-line comment.
+     */
+    singleLineComment?: SingleLineCommentFn;
+    /**
+     * Function that can format a given string array as a language-specific multi-line comment.
+     */
+    multiLineComment?: MultiLineCommentFn;
+    /**
+     * Function that can write out a start block.
+     */
+    startBlock?: StartBlockFn;
+    /**
+     * Function that can write out an end block.
+     */
+    endBlock?: EndBlockFn;
+}
+export declare type InitialCode = string | string[] | CodeWriter | undefined;
+export declare type SingleLineCommentFn = (cw: CodeWriter, comment: string) => void;
+export declare type MultiLineCommentFn = (cw: CodeWriter, comments: string[]) => void;
+export declare type StartBlockFn = (cw: CodeWriter, code?: string) => void;
+export declare type EndBlockFn = (cw: CodeWriter, code?: string) => void;
